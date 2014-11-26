a40724754ba8ee441f7a4eca6fc4e57c0af93df0
Vyacheslav Blinov <vblinov@unison.com>
2014-11-26 17:30:04 +0300

    update index sh script

---
+++ update-directory-index.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ROOT=$1
+
+if [ -d "$ROOT" ]; then
+  for DIR in $(find $ROOT -type d); do
+    (
+      echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
+      ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "^index\.html$" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
+      echo -e "</pre>\n</body>\n</html>"
+    ) > "${DIR}/index.html"
+  done
+else
+  echo "No such directory $ROOT"
+fi
